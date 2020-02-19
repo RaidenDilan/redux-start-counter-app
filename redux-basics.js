97a52a952a40888141f5c2ab96308942b85bee21
RaidenDilan <raiden18@me.com>
2020-02-19 00:04:18 +0000

    Adding Subscriptions

--- redux-basics.js
+++ redux-basics.js
@@ -5,7 +5,7 @@ const initialState = {
   counter: 0
 };
 
-// REDUCER
+// REDUCER - Runs First
 // state = initialState => default value of initialState
 const rootReducer = (state = initialState, action) => {
   if (action.type === 'INC_COUNTER') return {
@@ -20,16 +20,21 @@ const rootReducer = (state = initialState, action) => {
   return state;
 };
 
-// STORE
+// STORE - Runs 2nd
 const store = createStore(rootReducer);
 
 console.log('store.getState() 1st', store.getState());
 
-// DISPATCHING ACTION
+// SUBSCRIPTION - Runs 3rd
+// - is triggered whenever an action is patched
+store.subscribe(() => {
+  console.log('[Subscription]', store.getState());
+});
+
+// DISPATCHING ACTION Runs Last
 store.dispatch({ type: 'INC_COUNTER' });
 store.dispatch({ type: 'ADD_COUNTER', value: 10 });
 // store.dispatch({ type: 'SUB_COUNTER' });
 // store.dispatch({ type: 'DEC_COUNTER' });
 
 console.log('store.getState() 2nd', store.getState());
-// SUBSCRIPTION
